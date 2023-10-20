501bcfadc6b4306b7b20f349d0f3e657f2dfe9c3
nick <tjddndchl134@gmail.com>
2023-10-20 15:35:36 +0900

    자르파일 생성, 스프링부트 프로젝트 시작

---
+++ workspace_spring/bootPratice/src/main/java/com/example/demo/BootPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPraticeApplication.class, args);
+	}
+
+}
